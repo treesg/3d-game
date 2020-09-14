34880b656329c2ca9858d79d42a3f418ae3ae0e3
Sebastian Grana <sebastiangrana1@gmail.com>
2020-09-14 12:31:43 -0400

    node compatibility

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const http = require("http").Server(app);
-const io = require("socket.io")(http);
 const port = process.env.PORT || 2002;
 
 app.use(express.static("public"));
